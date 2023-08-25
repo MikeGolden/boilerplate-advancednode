d6fe366b48666a9a05f192735d06673cc77d8f68
Mike Goldenberg <csrutil@tuta.io>
2023-08-25 08:51:42 +0200

    correct auth strategy

---
+++ auth.js
@@ -0,0 +1,65 @@
+require('dotenv').config();
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const GitHubStrategy = require('passport-github').Strategy;
+const bcrypt = require('bcrypt');
+const { ObjectID } = require('mongodb');
+
+
+module.exports = function (app, myDataBase) {
+
+    passport.use(new LocalStrategy((username, password, done) => {
+        myDataBase.findOne({ username }, (error, user) => {
+          console.log(`User ${username} attempted to log in.`);
+          if (error) return done(error);
+          if (!user) return done(null, false);
+          if (!bcrypt.compareSync(password, user.password)) return done(null, false);
+          return done(null, user);
+        });
+      }));
+
+    passport.use(new GitHubStrategy({ 
+        clientID: process.env.GITHUB_CLIENT_ID, 
+        clientSecret: process.env.GITHUB_CLIENT_SECRET, 
+        callbackURL: 'https://oyster-app-kh8q5.ondigitalocean.app/auth/github/callback'
+    }, (accessToken, refreshToken, profile, cb) => {
+        console.log(profile);
+        myDataBase.findOneAndUpdate(
+            { id: profile.id },
+            {
+              $setOnInsert: {
+                id: profile.id,
+                username: profile.username,
+                name: profile.displayName || 'John Doe',
+                photo: profile.photos[0].value || '',
+                email: Array.isArray(profile.emails)
+                  ? profile.emails[0].value
+                  : 'No public email',
+                created_on: new Date(),
+                provider: profile.provider || ''
+              },
+              $set: {
+                last_login: new Date()
+              },
+              $inc: {
+                login_count: 1
+              }
+            },
+            { upsert: true, new: true },
+            (err, doc) => {
+              return cb(null, doc.value);
+            }
+          );
+    }));
+    
+      passport.serializeUser((user, done) => {
+        done(null, user._id);
+      });
+      
+      passport.deserializeUser((id, done) => {
+        myDataBase.findOne({ _id: new ObjectID(id) }, (error, doc) => {
+          done(null, doc);
+        });
+      });
+
+};
